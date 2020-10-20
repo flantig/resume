d525d4f5fecc15dbb1bccbaa4f35b6b78a67c581
Tacobell <franklinlantigua90@gmail.com>
2020-10-20 15:38:58 -0400

    -Changed the linkedin, github, and pdf icons to be about opacity
    -reworked how it passes props for panels

---
+++ src/components/Name.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="nameStyle">
+    <h1 :style="h1stylename">Franklin Lantigua</h1>
+    <img v-for="item in logoImages" v-bind:key="item.id" v-bind:src="item.src" alt="Not found"
+         v-on:click="item.openTab()" v-on:mousedown="item.imgStyle.opacity = .3"
+         v-on:mouseup="item.imgStyle.opacity = 1" :style="item.imgStyle">
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'Name',
+  data() {
+    return {
+      h1stylename: {
+        color: "floralwhite",
+        fontSize: "300%",
+      },
+      logoImages: [{
+        id: 1, src: require("../assets/headerImages/linkedin.webp"), openTab: function () {
+          window.open("https://www.linkedin.com/in/franklin-lantigua-06831aba/")
+        }, imgStyle: {width: "9%", margin: "5px", opacity: 1}
+      }, {
+        id: 2, src: require("../assets/headerImages/pdf.png"), openTab: function () {
+          window.open("https://drive.google.com/file/d/164LEDfctEf2hm2lvF91Vrde7i3ThSNwK/view?usp=sharing")
+        }, imgStyle: {width: "9%", margin: "5px", opacity: 1}
+      }, {
+        id: 3, src: require("../assets/headerImages/gitcat.png"), openTab: function () {
+          window.open("https://github.com/flantig")
+        }, imgStyle: {width: "9%", margin: "5px", opacity: 1}
+      }]
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+#nameStyle {
+  text-align: center;
+
+  width: 50%;
+  height: auto;
+  margin: 0px auto;
+  margin-top: 10%;
+  border-radius: 10px;
+  list-style: none;
+  padding-bottom: 1%;
+  margin-bottom: 10%;
+}
+</style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li id="divstyle" v-for="items in panelItems" v-bind:key="items.title" v-on:click="panelOpener = false">
+      <h1 id="h1style">{{ items.title }}</h1>
+      <img id="imgStyle" :src="items.src">
+
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'Panel',
+
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    panelItems: {
+      required: true,
+      type: Object
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+#imgStyle {
+  width: 25%;
+  left: initial;
+  margin-bottom: -.4%;
+  border-radius: 10px;
+}
+
+#h1style {
+  color: floralwhite;
+  font-size: 200%;
+  align-self: center;
+  float: right;
+  padding-right: 5%;
+}
+
+#divstyle {
+  background-color: #444444;
+  width: 60%;
+  height: auto;
+  margin: 0px auto;
+  margin-top: 2%;
+  border-radius: 10px;
+  list-style: none;
+}
+</style>
