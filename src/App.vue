4e80a2a122dbdc73b620307fd0195a91690a3792
Tacobell <franklinlantigua90@gmail.com>
2020-10-20 06:20:01 -0400

    -Changed the linkedin, github, and pdf icons to be about opacity
    -reworked how it passes props for panels

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app">
+
+    <Name/>
+    <Panel :panelItems="panelItems"/>
+
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Panel from "@/components/Panel";
+import Name from "@/components/Name";
+
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      panelItems: [
+        {
+          title: "Full Stack Energy Database",
+          src: require("./assets/panelImages/owl.png"),
+          open: false
+        },
+        {
+          title: "VBA Scripts",
+          src: require("./assets/panelImages/vbascript.png"),
+          open: true
+        },
+        {
+          title: "Selenium Automation",
+          src: require("./assets/panelImages/selenium.png"),
+          open: false
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Name,
+    Panel,
   }
 }
 </script>
 
 <style>
+html {
+  height: 100%
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #222222;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="divstyle" >
+<h1>Hi</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "Modal",
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+#divstyle {
+  background-color: coral;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
